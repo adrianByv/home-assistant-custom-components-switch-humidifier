c8b7ba8650954bcbd77304588e294b42a0970722
Myeongwon Choi <auspic7@gmail.com>
2021-07-06 17:40:15 +0900

    Fix adrianByv#8
    This commit will add new condition
    checks if the power state of switch is changed.

--- custom_components/switch_humidifier/humidifier.py
+++ custom_components/switch_humidifier/humidifier.py
@@ -205,7 +205,8 @@ class SwitchHumidifier(HumidifierEntity):
 
   def _state_changed(self, entity_id, old_state, new_state):
     """Called on sensor or switch state change"""
-    if not new_state is None and not new_state.state == 'unknown' and not new_state.state == 'unavailable':
+    if not new_state is None and not new_state.state == 'unknown' and not new_state.state == 'unavailable' and old_state.state != new_state.state:
+      # old switch power and new switch power must be different
       if entity_id == self._sensor_id:
         # Update humidity value
         self._humidity = float(new_state.state)
